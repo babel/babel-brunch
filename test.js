1420f04b5424ee13388d6811ceef52f1e59f5801
Elan Shanker <elan.shanker@gmail.com>
2014-10-01 20:21:34 -0400

    Don't worry about trailing newlines

--- test.js
+++ test.js
@@ -18,11 +18,11 @@ describe('Plugin', function() {
 
   it('should compile and produce valid result', function(done) {
     var content = 'let a = 1';
-    var expected = '(function () {\n  var a = 1;\n}());\n';
+    var expected = '(function () {\n  var a = 1;\n}());';
 
     plugin.compile({data: content, path: 'file.js'}, function(error, result) {
       assert(!error);
-      assert.equal(result.data, expected);
+      assert.equal(result.data.trim(), expected);
       done();
     });
   });
@@ -31,11 +31,11 @@ describe('Plugin', function() {
     plugin = new Plugin({sourceMaps: true});
 
     var content = 'let a = 1';
-    var expected = '(function () {\n  var a = 1;\n}());\n';
+    var expected = '(function () {\n  var a = 1;\n}());';
 
     plugin.compile({data: content, path: 'file.js'}, function(error, result) {
       assert(!error);
-      assert.equal(result.data, expected);
+      assert.equal(result.data.trim(), expected);
       assert.doesNotThrow(function(){JSON.parse(result.map)});
       done();
     });
